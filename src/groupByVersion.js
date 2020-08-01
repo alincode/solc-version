187a899f3a58edb4c4a0b30b2ae8d5388caafe64
alincode <alin.code@gmail.com>
2020-08-01 17:19:25 +0800

    fix: test case

--- src/groupByVersion.js
+++ src/groupByVersion.js
@@ -1,15 +1,13 @@
 module.exports = groupByVersion
 
+function filterCondition(x) {
+  return !~x.indexOf('v0.5.') && !~x.indexOf('v0.6.') && !~x.indexOf('v0.7.')
+}
+
 function removeAllZeroPointFiveVersion(select) {
-  select.nightly = select.nightly.filter(
-    x => !~x.indexOf('v0.5.') && !~x.indexOf('v0.6.')
-  )
-  select.all = select.all.filter(
-    x => !~x.indexOf('v0.5.') && !~x.indexOf('v0.6.')
-  )
-  select.releases = select.releases.filter(
-    x => !~x.indexOf('v0.5.') && !~x.indexOf('v0.6.')
-  )
+  select.nightly = select.nightly.filter((x) => filterCondition(x))
+  select.all = select.all.filter((x) => filterCondition(x))
+  select.releases = select.releases.filter((x) => filterCondition(x))
 }
 
 function groupByVersion(data, skip5 = true) {
