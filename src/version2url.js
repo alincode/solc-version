8bb5c7c571682f230399557f9cc31d517b199620
alincode <alin.code@gmail.com>
2023-08-28 00:33:00 +0800

    remove lodash dependencie

--- src/version2url.js
+++ src/version2url.js
@@ -2,7 +2,6 @@ const baseURL = 'https://solc-bin.ethereum.org/bin'
 
 const processList = require('./processList')
 const getlist = require('./getlist')
-const _ = require('lodash')
 
 module.exports = version2url
 
@@ -12,9 +11,13 @@ function version2url(version, list) {
       let data = list ? list : await getlist()
       let select = processList(data)
       const { all, releases } = select
-      if (version === 'stable') version = _.keys(releases).reverse()[0]
-      if (version === 'latest' || version === 'nightly')
-        version = _.keys(all).reverse()[0]
+      if (version === 'stable') {
+        version = Object.keys(releases).reverse()[0]
+      }
+      if (version === 'latest' || version === 'nightly') {
+        version = Object.keys(all).reverse()[0]
+      }
+
       var path = all[version]
       if (!path) return reject(new Error(`unknown version: ${version}`))
       resolve(`${baseURL}/${path}`)
